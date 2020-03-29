6957bb314183494b481bb4b9ab6eec374a62dd0b
ikenna agulobi <ikennaww@yahoo.fr>
2020-03-29 19:31:29 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,30 +1,42 @@
 
 katz_deli= []
+
 def line (katz_deli)
-  deli_line = "the line is currently:"
+  deli_line = "The line is currently: "
   if katz_deli.length==0
-    puts "the line is currently empty."
+    puts "The line is currently empty."
   else 
-    katz_deli.each_with_index do |ike, alex|
-    deli_line += "#{alex+1}. #{ike}"
+    katz_deli.each_with_index do |x , y|
+    deli_line += "#{y+1}. #{x} "
     end 
-    puts deli_line
+    puts deli_line.strip
 
   end 
 end 
 
-def take_a_number (katz_deli, person )
-  katz_deli.push(person)
-  puts "welcome , #{person}. You are number #{katz_deli.length} in the line."
+=begin
+ def take_a_number (katz_deli, person )
+   katz_deli.push(person)
+  puts "Welcome, #{person}. You are number #{katz_deli.length} in line."
+ end 
+
+=end 
+
+def take_a_number #(katz_deli)
+  katz_deli=[]
+  count=0 
+  count += 1
+  return "welcome #{katz_deli[0]} your ticket number is #{count}" 
 end 
+
 
  
 def now_serving (katz_deli)
-  if katz_deli.lenght==0
-    puts "there is nobody waiting to be served!"
+  if katz_deli.length==0
+    puts "There is nobody waiting to be served!"
   else
 
-    puts "currently serving  #{katz_deli[0]}. "
+    puts "Currently serving #{katz_deli[0]}."
     katz_deli.shift
   end 
 end 
